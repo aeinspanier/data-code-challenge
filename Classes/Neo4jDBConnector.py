fd647514c668c6568f5b759751a43313cac9e00b
Andrew Einspanier <aeinspanier23@gmail.com>
2023-03-16 08:24:51 -0500

    Finished writing queries

--- Classes/Neo4jDBConnector.py
+++ Classes/Neo4jDBConnector.py
@@ -10,22 +10,60 @@ class Neo4jDBConnector():
         # Don't forget to close the driver connection when you are finished with it
         self.driver.close()
 
-    def create_friendship(self, person1_name, person2_name):
-        with self.driver.session(database="neo4j") as session:
-            # Write transactions allow the driver to handle retries and transient errors
-            result = session.execute_write(
-                self._create_and_return_friendship, person1_name, person2_name)
-            for row in result:
-                print("Created friendship between: {p1}, {p2}".format(p1=row['p1'], p2=row['p2']))
-
     def load_protein_data(self, data):
         with self.driver.session(database="neo4j") as session:
-            # Write transactions allow the driver to handle retries and transient errors
-            result = session.execute_write(
-                self._create_protein, data.get('protein'))
+            # First, we create the protein
+            protein_id = data.get('protein')
+            organism = data.get('organism')
+            full_names = data.get('full_names')
+            references = data.get('references')
+            features = data.get('features')
+            genes = data.get('genes')
+            
+            
+            result = session.execute_write(self._create_protein, protein_id)
+            print(f"Created protein {result[0]}")
+            
+            result = session.execute_write(self._create_organism_relationship,
+                                            protein_id, organism.get('name'), organism.get('id'))
             for row in result:
-                print(f"Created protein {row}")
-        
+                print(f"created organism: {row.get('o')} for protein {row.get('p')}")
+            
+            for fn in full_names:
+                result = session.execute_write(self._create_full_name_relationship,
+                                            protein_id, fn)
+                for row in result:
+                    print(f"created full name: {row.get('fn')} for protein {row.get('p')}")
+                
+            
+            for ref in references:
+                result = session.execute_write(self._create_reference_relationship,
+                                            protein_id, ref.get('id_pubmed'), ref.get('id_doi'), 
+                                            ref.get('type'), ref.get('name'))
+                for row in result:
+                    print(f"created reference: {row.get('ref')} for protein {row.get('p')}")
+                
+                authors = ref.get('authors')
+                if authors:
+                    for author in authors:
+                        result = session.execute_write(self._create_author_relationship,
+                                            protein_id, ref.get('name'), author)
+                        for row in result:
+                            print(f"created author: {row.get('a')} for reference {row.get('ref')} for protein {row.get('p')}")
+            
+            for feat in features:
+                result = session.execute_write(self._create_feature_relationship,
+                                            protein_id, feat.get('name'), feat.get('type'), feat.get('position'),
+                                            feat.get('begin_position'), feat.get('end_position'))
+                for row in result:
+                    print(f"created feature: {row.get('f')} for protein {row.get('p')}")
+            
+            for gene in genes:
+                result = session.execute_write(self._create_gene_relationship,
+                                            protein_id, gene.get('name'), gene.get('status'))
+                for row in result:
+                    print(f"created gene: {row.get('g')} for protein {row.get('p')}")
+              
     @staticmethod
     def _create_protein(tx, protein_id):
         query = (
@@ -42,61 +80,106 @@ class Neo4jDBConnector():
             raise
     
     @staticmethod
-    def _create_organism_relationship():
-        pass
-    
-    @staticmethod
-    def _create_full_name_relationship():
-        pass
-    
-    @staticmethod
-    def _create_reference_relationship():
-        pass
-    
-    @staticmethod
-    def _create_author_relationship():
-        pass
-    
-    @staticmethod
-    def _create_feature_relationship():
-        pass
-    
-    @staticmethod
-    def _create_gene_relationship():
-        pass
-
-    @staticmethod
-    def _create_and_return_friendship(tx, person1_name, person2_name):
-        # To learn more about the Cypher syntax, see https://neo4j.com/docs/cypher-manual/current/
-        # The Reference Card is also a good resource for keywords https://neo4j.com/docs/cypher-refcard/current/
+    def _create_organism_relationship(tx, protein_id, organism_name, taxonomy_id):
         query = (
-            "CREATE (p1:Person { name: $person1_name }) "
-            "CREATE (p2:Person { name: $person2_name }) "
-            "CREATE (p1)-[:KNOWS]->(p2) "
-            "RETURN p1, p2"
+            "MATCH (p:Protein { id: $protein_id })"
+            "CREATE (o:Organism { name: $organism_name, taxonomy_id: $taxonomy_id }) "
+            "CREATE (p)-[:IN_ORGANISM]->(o) "
+            "RETURN p, o"
         )
-        result = tx.run(query, person1_name=person1_name, person2_name=person2_name)
+        result = tx.run(query, protein_id=protein_id, organism_name=organism_name, taxonomy_id=taxonomy_id)
         try:
-            return [{"p1": row["p1"]["name"], "p2": row["p2"]["name"]}
-                    for row in result]
+            return [{"p": row["p"]["id"], "o": row["o"]["name"]} for row in result]
         # Capture any errors along with the query and data for traceability
         except ServiceUnavailable as exception:
             logging.error("{query} raised an error: \n {exception}".format(
                 query=query, exception=exception))
             raise
-
-    def find_person(self, person_name):
-        with self.driver.session(database="neo4j") as session:
-            result = session.execute_read(self._find_and_return_person, person_name)
-            for row in result:
-                print("Found person: {row}".format(row=row))
-
+    
     @staticmethod
-    def _find_and_return_person(tx, person_name):
+    def _create_full_name_relationship(tx, protein_id, full_name):
         query = (
-            "MATCH (p:Person) "
-            "WHERE p.name = $person_name "
-            "RETURN p.name AS name"
+            "MATCH (p:Protein { id: $protein_id })"
+            "CREATE (fn:FullName { name: $full_name })"
+            "CREATE (p)-[:HAS_FULL_NAME]->(fn)"
+            "RETURN p, fn"
         )
-        result = tx.run(query, person_name=person_name)
-        return [row["name"] for row in result]
+        result = tx.run(query, protein_id=protein_id, full_name=full_name)
+        try:
+            return [{"p": row["p"]["id"], "fn": row["fn"]["name"]} for row in result]
+        # Capture any errors along with the query and data for traceability
+        except ServiceUnavailable as exception:
+            logging.error("{query} raised an error: \n {exception}".format(
+                query=query, exception=exception))
+            raise
+    
+    @staticmethod
+    def _create_reference_relationship(tx, protein_id, ref_id_pubmed, ref_id_doi, ref_type, ref_name):
+        query = (
+            "MATCH (p:Protein { id: $protein_id })"
+            "CREATE (ref:Reference { id_pubmed: $ref_id_pubmed, id_doi: $ref_id_doi, type: $ref_type, name: $ref_name })"
+            "CREATE (p)-[:HAS_REFERENCE]->(ref)"
+            "RETURN p, ref"
+        )
+        result = tx.run(query, protein_id=protein_id, ref_id_pubmed=ref_id_pubmed, 
+                        ref_id_doi=ref_id_doi, ref_type=ref_type, ref_name=ref_name)
+        try:
+            return [{"p": row["p"]["id"], "ref": row["ref"]["name"]} for row in result]
+        # Capture any errors along with the query and data for traceability
+        except ServiceUnavailable as exception:
+            logging.error("{query} raised an error: \n {exception}".format(
+                query=query, exception=exception))
+            raise
+    
+    @staticmethod
+    def _create_author_relationship(tx, protein_id, ref_name, author):
+        query = (
+            "MATCH (p:Protein { id: $protein_id}) -- (ref:Reference { name: $ref_name })"
+            "CREATE (a:Author { name: $author })"
+            "CREATE (ref)-[:HAS_AUTHOR]->(a)"
+            "RETURN p, ref, a"
+        )
+        result = tx.run(query, protein_id=protein_id, ref_name=ref_name, author=author)
+        try:
+            return [{"p": row["p"]["id"], "ref": row["ref"]["name"], "a": row['a']['name']} for row in result]
+        # Capture any errors along with the query and data for traceability
+        except ServiceUnavailable as exception:
+            logging.error("{query} raised an error: \n {exception}".format(
+                query=query, exception=exception))
+            raise
+    
+    @staticmethod
+    def _create_feature_relationship(tx, protein_id, feat_name, feat_type, feat_position,
+                                            feat_begin_position, feat_end_position):
+        query = (
+            "MATCH (p:Protein { id: $protein_id})"
+            "CREATE (f:Feature { name: $feat_name, type: $feat_type  })"
+            "CREATE (p)-[:HAS_FEATURE { position: $feat_position, begin_position: $feat_begin_position, end_position: $feat_end_position} ]->(f)"
+            "RETURN p, f"
+        )
+        result = tx.run(query, protein_id=protein_id, feat_name=feat_name, feat_type=feat_type, feat_position=feat_position,
+                                            feat_begin_position=feat_begin_position, feat_end_position=feat_end_position)
+        try:
+            return [{"p": row["p"]["id"], "f": row["f"]["name"]} for row in result]
+        # Capture any errors along with the query and data for traceability
+        except ServiceUnavailable as exception:
+            logging.error("{query} raised an error: \n {exception}".format(
+                query=query, exception=exception))
+            raise
+    
+    @staticmethod
+    def _create_gene_relationship(tx, protein_id, name, status):
+        query = (
+            "MATCH (p:Protein { id: $protein_id})"
+            "CREATE (g:GENE { name: $name })"
+            "CREATE (p)-[:FROM_GENE { status: $status} ]->(g)"
+            "RETURN p, g"
+        )
+        result = tx.run(query, protein_id=protein_id, name=name, status=status)
+        try:
+            return [{"p": row["p"]["id"], "g": row["g"]["name"]} for row in result]
+        # Capture any errors along with the query and data for traceability
+        except ServiceUnavailable as exception:
+            logging.error("{query} raised an error: \n {exception}".format(
+                query=query, exception=exception))
+            raise
--- dags/pipeline.py
+++ dags/pipeline.py
@@ -11,7 +11,11 @@ from Classes.Neo4jDBConnector import Neo4jDBConnector
 
 @dag(schedule=None, start_date=pendulum.datetime(2021, 1, 1, tz="UTC"), catchup=False)
 def protein_pipeline():
-    file_path = f'{os.getcwd()}/dags/data/Q9Y261.xml'
+    ## [USER DEFINED VALUE] ##
+    file_name = 'Q9Y261.xml'
+    ## [USER DEFINED VALUE] ##
+    
+    file_path = f'{os.getcwd()}/dags/data/{file_name}'
     with open(file_path, 'r') as f:
         data = f.read()
 
@@ -22,9 +26,11 @@ def protein_pipeline():
     
     @task()
     def load(data: dict):
+        ## [USER DEFINED VALUES] ##
         uri = "bolt://host.docker.internal:7687"
         user = "superman"
         password = "testing123"
+        ## [USER DEFINED VALUES] ##
         neo4j = Neo4jDBConnector(uri, user, password)
         neo4j.load_protein_data(data)
 
